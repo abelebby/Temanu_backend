67f30593b22ab8a2b8ddfadb2bd8a46d52c1cb90
@RyanGKC <ryangoh8228@gmail.com>
2026-04-02 17:59:39 +0800

    Fixed a bug where there was 2 mail ports

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ mail_config = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_EMAIL"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_EMAIL"),
-    MAIL_PORT=587,
     MAIL_PORT=465,
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=True,
